9c6bf22c19dbbf9ce15eb7a6e2cb0ed6757bd0c1
עומר נחום <oomer784@hotmail.com>
2017-02-18 11:09:16 +0200

    registerpage

--- Register_Page.aspx
+++ Register_Page.aspx
@@ -4,13 +4,13 @@
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="Body" runat="Server">
     <div id="register">
-        <input id="userbox" type="text" placeholder="Username" />
+        <input id="userbox" type="text" placeholder="Username" runat="server"/>
         <br />
-        <input id="passbox" type="password" placeholder="Password" />
+        <input id="passbox" type="password" placeholder="Password" runat="server" title="A password requires, at least one uppercase letter, a lowercase letter, and a digit" pattern="(?=.*[A-Z])(?=.*[a-z])(?=.*\d)[0-9a-zA-Z!-+]{8,}" />
         <br />
-        <input id="passboxcon" type="password" placeholder="Confirm Password" />
+        <input id="passboxcon" type="password" placeholder="Confirm Password" runat="server" />
         <br />
-        <input id="subbut" type="submit" value="Register" />
+      <input id="subbut" type="submit" value="Register" runat="server" onserverclick="subbut_ServerClick" />
     </div>
 </asp:Content> 
 
--- Register_Page.aspx.cs
+++ Register_Page.aspx.cs
@@ -7,24 +7,59 @@ using System.Web.UI.WebControls;
 using System.Data.SqlClient;
 using System.Configuration;
 using System.Data;
+using System.Text.RegularExpressions;
 
 public partial class Default2 : System.Web.UI.Page
 {
+    private List<string> usernames;
+    private List<string> passwords;
     protected void Page_Load(object sender, EventArgs e)
     {
         string connectionstring = ConfigurationManager.ConnectionStrings["DatabaseConnectionString"].ConnectionString;
         using (SqlConnection connection = new SqlConnection(connectionstring))
-        using (SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM [Table];",connection))
+        using (SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM [Table];", connection))
         {
             DataTable users = new DataTable();
             adapter.Fill(users);
-            List<string> usernames = users.AsEnumerable().Select(x => x[1].ToString()).ToList();//list of all usernames
-            List<string> passwords = users.AsEnumerable().Select(x => x[2].ToString()).ToList();//list of all usernames
+            usernames = users.AsEnumerable().Select(x => x[1].ToString()).ToList();//list of all usernames
+            passwords = users.AsEnumerable().Select(x => x[2].ToString()).ToList();//list of all passwords       
+        }
+    }
+    protected void subbut_ServerClick(object sender, EventArgs e)
+    {
+        ClientScriptManager manager = Page.ClientScript;
+        string username = userbox.Value;
+        string password = passbox.Value;
+        string passwordconfirm = passboxcon.Value;
+        Regex regex = new Regex(@"(?=.*[A-Z])(?=.*[a-z])(?=.*\d)[0-9a-zA-Z!-+]{8,}");
+        Match match = regex.Match(password);
 
-          
+        if (match.Success)
+        {
+            if (password == passwordconfirm)
+            {
+                string query = string.Format("INSERT INTO [Table] (username,password) VALUES ('{0}','{1}');", username, password);
+                string connectionstring = ConfigurationManager.ConnectionStrings["DatabaseConnectionString"].ConnectionString;
+                using (SqlConnection connection = new SqlConnection(connectionstring))
+                using (SqlCommand command = new SqlCommand(query, connection))
+                using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+                {
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+                Alert("Registered");
+            }
+            else
+            {
+                Alert("Passwords have to match");
+            }
+
         }
 
 
-
+    }
+    private void Alert(string message)
+    {
+        Page.ClientScript.RegisterStartupScript(this.GetType(), "Scripts", string.Format("<script> alert('{0}'); </script>", message));
     }
 }
